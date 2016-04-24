842aa13140f0410127f513d9b419a06cc509a2e7
Sebastian B. Knapp <rock@ccls-online.de>
2016-04-24 11:24:27 +0200

    logging++

--- recreation/yourmap/urls.py
+++ recreation/yourmap/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^new-geo-tag$', views.add_geotag, name='add_geotag'),
+    url(r'^edit-geo-tag$', views.edit_geotag, name='edit_geotag'),
 ]
--- recreation/yourmap/views.py
+++ recreation/yourmap/views.py
@@ -8,16 +8,25 @@ from django.shortcuts import get_object_or_404, render, redirect
 # Get an instance of a logger
 logger = logging.getLogger("django." + __name__)
 
-# Create your views here.
+# The map with active geotags
 def index(request):
     #logger.info(dir(request.user))
-    if not request.user.is_authenticated():
-        logger.info(request.user.__class__)
-        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+    #if not request.user.is_authenticated():
+    #    logger.info(request.user.__class__)
+    #    return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     return render(request, 'yourmap/index.html')
 
+# The application landing page (we will see what happens here)
 def home(request):
     return render(request, 'yourmap/home.html')
+
+# Create a new GeoTag
+def add_geotag(request):
+    return render(request, 'yourmap/add_geotag.html')
+
+# Edit a GeoTag - can only be done by the creator of the tag
+def edit_geotag(request):
+    return render(request, 'yourmap/edit_geotag.html')
 
 #    def results(request, question_id):
 #        question = get_object_or_404(Question, pk=question_id)
