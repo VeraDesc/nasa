56389cb3c8df63339158d1942dd2cb75687266c4
Sebastian B. Knapp <rock@ccls-online.de>
2017-05-19 17:18:03 +0200

    add models to admin area

--- recreation/yourmap/admin.py
+++ recreation/yourmap/admin.py
@@ -1,3 +1,8 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.GeoTag)
+admin.site.register(models.MediaType)
+admin.site.register(models.Media)
--- recreation/yourmap/models.py
+++ recreation/yourmap/models.py
@@ -7,12 +7,19 @@ from django.db import models
 
 class GeoTag(models.Model):
     title = models.CharField(max_length=512)
-    lat = models.DecimalField(max_digits=12,decimal_places=9)
-    lng = models.DecimalField(max_digits=12,decimal_places=9)
-    alt = models.DecimalField(max_digits=12,decimal_places=7,default=0.0)
+    lat = models.DecimalField(max_digits=8,decimal_places=4)
+    lng = models.DecimalField(max_digits=8,decimal_places=4)
+    alt = models.DecimalField(max_digits=8,decimal_places=5,default=0.0)
+
+    def __str__(self):
+        return self.title
 
 class MediaType(models.Model):
     code = models.CharField(max_length=24)
+
+    def __str__(self):
+        return self.code
+
 
 class Media(models.Model):
     type = models.ForeignKey(MediaType, on_delete=models.CASCADE)
